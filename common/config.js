62b2fba495b4485f03076b8490a9699b8269e7b8
793073861@qq.com <793073861@qq.com>
2022-01-12 15:25:25 +0800

    product

--- common/config.js
+++
@@ -1,6 +0,0 @@
-// const Api_url= 'https://sensumi.top' 
-const Api_url= 'http://localhost:8181'
-// const Api_url= 'http://10.36.36.251:8181' 
-// const Api_url= 'http://192.168.3.54:8181' 
-// const Api_url = '/api'
-export {Api_url}
